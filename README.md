5ffb11d216ef984513017ca08e6fa56a09b00a41
alxnsk <xanoral@gmail.com>
2025-05-06 19:11:39 +0500

    Initial commit 2, I guess XD

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple-image-converter
-This FoundryVTT module converts any image uploaded by non-GM users to WebP format and places it in a specified directory. It can optionally scale the image proportionally
+No more "UltraHiRez 72Mb in size" png files! No more, or at least less: pleas, tears, treats or social contracts during the pre-planning stage of your TRPG campaign!
+
+This FoundryVTT module replaces the standard FilePicker window for non-GM users with a simple drag-and-drop interface, which converts any provided image to WebP format on the client side and saves it in a location specified by the GM.
+
+All that, and it still looks less complicated than airplane controls, even for users who aren’t that tech-savvy.
+
+Simple.
---
+++ scripts/converter.js
@@ -0,0 +1,193 @@
+Hooks.once('init', () => {
+    const originalBrowse = FilePicker.prototype.browse;
+    FilePicker.prototype.browse = async function () {
+        const forced = game.settings.get("simple-image-converter", "enabledForGm") || false;
+
+        const hijackModes = {
+            "imagevideo" : true,
+            "image"      : true
+        };
+
+        if( (!forced && game.user.isGM) || true !== hijackModes[this.type] ){
+            return originalBrowse.call(this, ...arguments);
+        }
+
+        const app = new WebPConverterApp();
+        app.render(true);
+        const path = await app.result;
+        // Update the target field
+        if ( this.field ) {
+            this.field.value = path;
+            this.field.dispatchEvent(new Event("change", {bubbles: true, cancelable: true}));
+        }
+        // Trigger a callback and close
+        if ( this.callback ) this.callback(path, this);
+        return this.close();
+        
+    }
+    
+    // Directory picker setting
+    game.settings.register("simple-image-converter", "uploadDirectory", {
+        name: game.i18n.localize("simple-image-converter.settings.uploadDirectoryName"),
+        hint: game.i18n.localize("simple-image-converter.settings.uploadDirectoryHint"),
+        scope: "world",
+        config: true,
+        default: "",
+        type: String,
+        filePicker: "folder"
+    });
+    
+    // Max side size in pixels.
+    game.settings.register("simple-image-converter", "maxSidePixelSize", {
+        name: game.i18n.localize("simple-image-converter.settings.maxSidePixelSizeName"),
+        hint: game.i18n.localize("simple-image-converter.settings.maxSidePixelSizeHint"),
+        scope: "world",
+        config: true,
+        default: 0,
+        type: Number
+    });
+    
+    // Compression rate percent
+    game.settings.register("simple-image-converter", "qualityPercent", {
+        name: game.i18n.localize("simple-image-converter.settings.qualityPercentName"),
+        hint: game.i18n.localize("simple-image-converter.settings.qualityPercentHint"),
+        scope: "world",
+        config: true,
+        type: new foundry.data.fields.NumberField({nullable: false, min: 10, max: 95, step: 5}),
+        default: 80
+    });
+    
+    game.settings.register("simple-image-converter", "enabledForGm", {
+        name: game.i18n.localize("simple-image-converter.settings.enabledForGmName"),
+        hint: game.i18n.localize("simple-image-converter.settings.enabledForGmHint"),
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+
+    game.settings.register("simple-image-converter", "generateRandomFileName", {
+        name: game.i18n.localize("simple-image-converter.settings.generateRandomFileNameName"),
+        hint: game.i18n.localize("simple-image-converter.settings.generateRandomFileNameHint"),
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+    
+});
+
+class WebPConverterApp extends FormApplication {
+    constructor(...args) {
+        super(...args);
+        this._resolver = null;
+        this._promise = new Promise(resolve => this._resolver = resolve);
+    }
+    
+    get result() {
+        return this._promise;
+    }
+    
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            id: "simple-image-converter",
+            title: "Simple image converter",
+            template: "modules/simple-image-converter/templates/ui.html"
+        });
+    }
+    
+    activateListeners(html) {
+        html.find("#upload").on("change", this._handleUpload.bind(this));
+        
+        const dropArea      = html.find("#drop-area")[0];
+        const previewCanvas = html.find("#preview-canvas")[0];
+        const inputUpload   = html.find("#upload")[0];
+
+        dropArea.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            dropArea.classList.add("hover");
+        });
+        
+        dropArea.addEventListener("dragleave", () => {
+            dropArea.classList.remove("hover");
+        });
+        
+        dropArea.addEventListener("drop", (e) => {
+            e.preventDefault();
+            //dropArea.classList.remove("hover");
+            dropArea.classList.add("hidden");
+            inputUpload.classList.add("hidden");
+            previewCanvas.classList.remove("hidden");
+            
+            const file = e.dataTransfer.files[0];
+            if (file) this._convertAndUpload(file);
+        });
+    }
+    
+    async _handleUpload(event) {
+        const file = event.target.files[0];
+        if (file) this._convertAndUpload(file);
+    }
+    
+    async _convertAndUpload(file) {
+        const img = new Image();
+        const reader = new FileReader();
+        
+        reader.onload = (e) => {
+            img.src = e.target.result;
+            document.getElementById("preview-canvas").src = img.src;
+            img.onload = async () => {
+                const settingSizeLimit = game.settings.get("simple-image-converter", "maxSidePixelSize") || 0;
+                const settingCompression = game.settings.get("simple-image-converter", "qualityPercent") || 80;
+                
+                let targetQuality = settingCompression / 100;
+                if(settingCompression < 10 || settingCompression > 100){
+                    targetQuality = 0.8;
+                }
+                
+                let targetWidth  = img.width;
+                let targetHeight = img.height;
+                
+                if( null != settingSizeLimit && settingSizeLimit > 0){
+                    let scale = 1;
+                    if(img.width > settingSizeLimit){
+                        scale = settingSizeLimit / img.width;  
+                    }else if(img.height > settingSizeLimit){
+                        scale = settingSizeLimit / img.height;
+                    }
+                    
+                    targetWidth  = Math.min( Math.trunc( targetWidth * scale ),  settingSizeLimit) ;
+                    targetHeight = Math.min( Math.trunc( targetHeight * scale ), settingSizeLimit);
+                }
+                
+                const canvas  = document.createElement("canvas");
+                canvas.width  = targetWidth;
+                canvas.height = targetHeight;
+                canvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height, 0,0, targetWidth, targetHeight);
+                
+                canvas.toBlob(async (blob) => {
+                    const safeUserName = WebPConverterApp.escapeUserName(game.user.name);
+                    //Name is random or no?
+                    const newFileName = true === game.settings.get("simple-image-converter", "generateRandomFileName") ? 
+                        safeUserName + "-" + foundry.utils.randomID(18) + ".webp" : safeUserName + "-" + file.name.replace(/\.\w+$/, ".webp");
+                    
+                    const webpFile = new File([blob], newFileName, { type: "image/webp" });
+                    let uploadPath = game.settings.get("simple-image-converter", "uploadDirectory") || "";
+
+                    
+                    const result = await FilePicker.upload("data", uploadPath, webpFile);
+                    debugger;
+                    if (this._resolver) this._resolver(result.path); // Resolve the promise with the file path
+                    this.close();
+                    
+                }, "image/webp", targetQuality);
+            };
+        };
+        
+        reader.readAsDataURL(file);
+    }
+
+    static escapeUserName(dirtyString){
+        return dirtyString.replace(/[^A-Za-z\d]/g, "x");
+    }
+}
